8dba4d39a7b9848dab8974aca0160715a4f5af6d
André Ricardo <andrericsouza@yahoo.com.br>
2024-09-26 22:18:45 -0300

    teste do black

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import socket
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     # Obter o nome da máquina e o IP
     hostname = socket.gethostname()
@@ -12,5 +12,5 @@ def index():
     return f"Hostname: {hostname}<br>IP Address: {ip_address}"
 
 
-if __name__ == "__main__":
+if __name__ ==  '__main__':
     app.run(host="0.0.0.0", port=5000)
